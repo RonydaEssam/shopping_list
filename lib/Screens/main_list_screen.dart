1f4afbe202f0f285d6dfcc1dc23678096e5533e9
RonydaEssam <ronyda988@gmail.com>
2025-11-16 22:03:31 +0200

    initial commit

---
+++ lib/Screens/main_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainListScreen extends StatelessWidget {
+  MainListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
